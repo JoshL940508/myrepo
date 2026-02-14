0431d0cbb39d72d74ca470f2a2c5670078f644d1
JoshL940508 <hlin37@student.ubc.ca>
2026-02-14 06:17:34 +0000

    Q1

--- Q1.c
+++ Q1.c
@@ -5,10 +5,7 @@
 #define ARRAY_SIZE 50000000
 
 void add_arrays(int* result, int* a, int* b, int array_size) {
-    // @TODO
-	// Add a + b together and store in the result array.
-    // DO NOT create any local variables in this function.
-    // +4
+  
     while (array_size--) {
         result[array_size] = a[array_size] + b[array_size];
     }
@@ -21,12 +18,7 @@ void rand_init(int* a, int array_size) {
     for (int i = 0; i < array_size; i++) {
         a[i] = (rand() % (range_end - range_start + 1)) + range_start;
     }
-    // @TODO
-    // Randomly initialize an array of size array_size
-    // using numbers between 0 and 9.
-    // +2
-
-
+   
 }
 
 int main(void) {
@@ -45,7 +37,6 @@ int* a = (int*)malloc(sizeof(int) * ARRAY_SIZE);
 
     srand((unsigned)time(NULL));
 
-    /* Time initialization of a and b */
     double start = clock();
     rand_init(a, ARRAY_SIZE);
     rand_init(b, ARRAY_SIZE);
@@ -53,12 +44,10 @@ int* a = (int*)malloc(sizeof(int) * ARRAY_SIZE);
 
     printf("Total init time: %.2f ms\n", 1000 * (stop - start) / CLOCKS_PER_SEC);
 
-    /* Time add */
     start = clock();
     add_arrays(c, a, b, ARRAY_SIZE);
     stop = clock();
 
-    /* Print first 10 values of c on one line (like the sample) */
     for (int i = 0; i < 10; i++) {
         printf("%d ", c[i]);
     }
@@ -70,27 +59,5 @@ int* a = (int*)malloc(sizeof(int) * ARRAY_SIZE);
     free(b);
     free(c);
     return 0;
-    // @TODO
-	// Allocate the source arrays (a, b, c)
-    // a and b are the source arrays.
-    // c is the result storage array.
-    // c[i] should equal a[i] + b[i]
-    // Make sure to error check.
-    // +1
-    
-    // @TODO
-    // Initialize the two arrays, a and b, with random values 
-    // of size ARRAY_SIZE and measure the time it takes to do so.
-    // +1
-
-    // @TODO
-    // Print the total time for initialization
-    // +1
-
-    // @TODO
-    // Run and time the add operation, print the first ten elements
-    // of the result array, and print the total add time.
-    // +2
-
-    return 0;
+   
 }
